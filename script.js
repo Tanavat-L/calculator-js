307d2bced66651b27b8f2ba335b77a82829121f4
Tanavat-L <102414255+Tanavat-L@users.noreply.github.com>
2024-05-20 18:03:24 +0700

    Wire buttons and show on display

--- script.js
+++ script.js
@@ -30,6 +30,50 @@ function operate(num1, operand, num2){
     }
 }
 
-let num1;
-let num2;
-let operand;
+function display(num1,operator,num2){
+    let display = document.querySelector(".display");
+    display.textContent = `${num1}${operator}${num2}`;
+}
+
+function wireButtons(){
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+    for(let i = 0; i < numbers.length; i++){
+        numbers[i].addEventListener("click", (e) =>{
+            if(operator === ''){
+                num1 += (i+1);
+            }
+            else{
+                num2 += (i+1);
+            }
+            display(num1,operator, num2);
+        });
+    }
+
+    for(let i = 0; i< operators.length; i++){
+        operators[i].addEventListener("click", (e) => {
+            if(num1){
+                switch (i){
+                    case 0:
+                        operator = '/';
+                        break;
+                    case 1:
+                        operator = '*';
+                        break;
+                    case 2:
+                        operator = '-';
+                        break;
+                    case 3: 
+                        operator = '+';
+                        break;
+                }    
+            }
+            display(num1,operator,num2);
+        })
+    }
+}
+
+let num1 ='';
+let num2 ='';
+let operator ='';
+wireButtons();
